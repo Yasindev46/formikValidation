7345b77e697f9e2ffb40931e2beb2aa21ddd5526
pickupbiz <43946129+pickupbiz@users.noreply.github.com>
2022-12-25 12:52:34 +0530

    Updated the changes to message

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,55 +1,70 @@
-import {Button, Card, CardContent, TextField,Grid } from "@mui/material"
-import React from "react"
-import { useState } from "react"
-import {validation} from "../Validation/LoginValidation"
+import { Button, Card, CardContent, TextField, Grid } from "@mui/material";
+import React from "react";
+import { useState } from "react";
+import { validation } from "../Validation/LoginValidation";
 
-export const Login=()=>{
+export const Login = () => {
+  let a = "";
+  const [error, setError] = useState({});
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
 
-    let a=""
-    const [error,setError]=useState({})
-    const [values,setValues]=useState({
-        email:"",
-        password:""
-    })
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setError(validation(values));
+  };
+  console.log(error, "===>");
 
-    const handleChange=(e)=>{
-        setValues({...values,[e.target.name]:e.target.value})
-    }
-    const handleSubmit=(e)=>{
-        e.preventDefault()
-        setError(validation(values))
-    }
-    console.log(error,"===>") 
+  return (
+    <React.Fragment>
+      <form onSubmit={handleSubmit}>
+        <Card style={{ width: "300px", marginLeft: "35%", marginTop: "100px" }}>
+          <CardContent align="center">
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <h1>Employee Login </h1>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  type="text"
+                  placeholder="Enter email"
+                  name="email"
+                  onChange={handleChange}
+                />
 
-    return(
-        <React.Fragment>
-            <form onSubmit={handleSubmit}>
-            <Card style={{width:"300px",marginLeft:"35%",marginTop:"100px"}}>
-                <CardContent align="center">
-                    <Grid container spacing={3}>
-                        <Grid item xs={12}>
-                            <h1>Employee Login </h1>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField variant="outlined" type="text" placeholder="Enter email" name="email" onChange={handleChange}/>
-
-                            {error && <p style={{color:"red"}}>{error.email}</p>}
-                            {/* {error?<p style={{color:"red"}}>{error.email}</p>:""} */}
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField variant="outlined" type="password" placeholder="Enter Password" name="password" onChange={handleChange}/>
-                            {error && <p style={{color:"red"}}>{error.password}</p>}
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Button variant="contained" type="submit">Login</Button>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <p>Dont have an account? <a href="">Register here</a></p>
-                        </Grid>
-                    </Grid>
-                </CardContent>
-            </Card>
-            </form>
-        </React.Fragment>
-    )
-}
+                {error && <p style={{ color: "red" }}>{error.email}</p>}
+                {/* {error?<p style={{color:"red"}}>{error.email}</p>:""} */}
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  type="password"
+                  placeholder="Enter Password"
+                  name="password"
+                  onChange={handleChange}
+                />
+                {error && <p style={{ color: "red" }}>{error.password}</p>}
+              </Grid>
+              <Grid item xs={12}>
+                <Button variant="contained" type="submit">
+                  Login
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <p>
+                  Don't have an account? <a href="">Register here</a>
+                </p>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </form>
+    </React.Fragment>
+  );
+};
